dff0108665fec75fece5adb5fbd9ed98020338bb
Seth Doty <seth.doty@protonmail.com>
2020-03-16 20:42:13 -0500

    update org copture template

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -7,7 +7,8 @@
  '(newsticker-url-list
    (quote
     (("Aws - Whats New?" "https://aws.amazon.com/new/feed/" nil nil nil)
-     ("Kubernetes Blog" "https://kubernetes.io/feed.xml" nil nil nil))))
+     ("Kubernetes Blog" "https://kubernetes.io/feed.xml" nil nil nil)
+     ("XKCD" "https://www.xkcd.com/atom.xml" nil nil nil))))
  '(package-selected-packages
    (quote
     (helm-mu helm-org-ql org-ql helm-rg yasnippet-snippets lsp-python-ms jedi blacken flymake-go company-go go-mode flycheck-rust cargo rust-mode dotenv-mode dockerfile-mode docker-compose-mode company-terraform terraform-mode yaml-mode cider fountain-mode company-lua lua-mode rainbow-delimiters geiser company-jedi python-mode impatient-mode simple-httpd rainbow-mode emmet-mode company-web web-mode json-mode prettier-js indium add-node-modules-path js2-refactor eslint-fix js2-mode company-lsp lsp-ui lsp-mode company-quickhelp gnuplot-mode gnuplot toc-org ob-browser ob-restclient diff-hl gitignore-mode git-link forge magit helm-company helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm olivetti vterm company-restclient restclient graphql-mode diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck switch-buffer-functions multiple-cursors ace-window htmlize dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar nyan-mode hydra dired-subtree undo-tree golden-ratio-scroll-screen use-package)))
