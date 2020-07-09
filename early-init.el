3d5bd1f56538bccea894607f904838cbc74cb0f9
Seth Doty <seth.doty@posteo.net>
2020-07-08 22:01:10 -0500

    Migrate to straight.el

--- early-init.el
+++
@@ -1,7 +0,0 @@
-;;; -*- lexical-binding: t -*-
-(setq package-enable-at-startup nil
-      tool-bar-mode nil
-      menu-bar-mode nil)
-(set-face-attribute 'default nil  :family "JetBrains Mono Medium" :height 80 :weight 'normal)
-(set-scroll-bar-mode nil)
-
